e81ea0aba6ebdc41e84b84755b619cab22ad9078
Hugo Otuszewski <hugo.otuszewski@gmail.com>
2024-10-10 09:47:04 +0200

    feat: :sparkles: API

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/fake-data", (req, res) => {
+	const fakeData = {
+		id: 1,
+		name: "John Doe",
+		email: "johndoe@example.com",
+		age: 30,
+		job: "Full Stack Developer",
+	};
+
+	res.json(fakeData);
+});
+
+app.listen(PORT, () => {
+	console.log(`Serveur lancé sur le port ${PORT}`);
+});
